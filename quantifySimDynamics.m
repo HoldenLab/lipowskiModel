e4df8f5dd978ecd2c0a31d379af3ed9a3f367cf8
seamusholden <seamus.holden@ncl.ac.uk>
2022-03-09 17:03:12 +0000

    initial attempt at implementing quantification and simulation repeats in single function
    Still needs testing
    Will allow running over different conditions/ concentrations

---
+++ quantifySimDynamics.m
@@ -0,0 +1,40 @@
+function [processivity, speed, lifetime, rate, fractionMotile, simResult] = quantifySimDynamics(motorState, simPar,analysisPar,nSimRepeat)
+
+simResult=table;
+for ii=1:nSimRepeat
+    [dynamics] = lipowskiWithUnbindingSim(motorState,simPar);
+    t=dynamics.t;
+    x=dynamics.x;
+    runID=ii;
+    [simResultCur] = analyseSimDynamics(t,x,frameInterval,zeroSpeedThresh, switchTooCloseThresh, runID);
+    simResult=[simResult;simResultCur];
+end
+
+%implement rate calculations as functions so can do bootstrap CIs
+nBoot = 1000;
+pause_processive_rateFcn= @(simResult) sum(simResult.isMotile==1 & simResult.isNextStateMotile==0)/ sum(simResult.duration(simResult.isMotile==1));
+pause_processive_rate=pause_processive_rateFcn(simResult);
+pause_processive_rate_ci = bootci(nBoot,pause_processive_rateFcn,simResult);
+rate.pause_processive = [pause_processive_rate, pause_processive_rate_ci];
+
+reversal_processive_rateFcn= @(simResult) sum(simResult.isMotile==1 & simResult.isNextStateMotile==1)/ sum(simResult.duration(simResult.isMotile==1));
+reversal_processive_rate=reversal_processive_rateFcn(simResult);
+reversal_processive_rate_ci=bootci(nBoot,reversal_processive_rateFcn,simResult);
+rate.reversal_processive = [reversal_processive_rate,reversal_processive_rate_ci];
+
+staticToMotile_rateFcn = @(simResult) sum(simResult.isMotile==0)/sum(simResult_static.duration);
+staticToMotile_rate = staticToMotile_rateFcn(simResult);
+staticToMotile_rate_ci=bootci(nBoot,staticToMotile_rateFcn,simResult);
+rate.staticToMotile = [staticToMotile_rate,staticToMotile_rate_ci];
+
+fractionMotileFcn = @(simResult) sum(simResult.duration(simResult.isMotile==1))/sum(simResult.duration);
+fractionMotile = fractionMotileFcn(simResult);
+fractionMotile_ci=bootci(nBoot,fractionMotileFcn,simResult);
+fractionMotile = [fractionMotile, fractionMotile_ci];
+
+lifetime.Total = simResult.duration;
+lifetime.Processive = simResult.duration(simResult.isMotile==1);
+lifetime.Static = simResult.duration(simResult.isMotile==0);
+
+
+
